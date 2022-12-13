24d492eedc9ca04b0c197b5bdc11d10f93f9eedf
yandi <prozhw@qq.com>
2022-12-13 15:24:05 +0800

    json-rpc服务中心

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,8 +6,8 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'a4a119a56e50fbb293281d9a48007e0e' => $vendorDir . '/symfony/polyfill-php80/bootstrap.php',
     'bbeb7603826cb9296dde3ca1a840af47' => $vendorDir . '/hyperf/utils/src/Functions.php',
+    'a4a119a56e50fbb293281d9a48007e0e' => $vendorDir . '/symfony/polyfill-php80/bootstrap.php',
     '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
     '6e3fae29631ef280660b3cdad06f25a8' => $vendorDir . '/symfony/deprecation-contracts/function.php',
     '320cde22f66dd4f5d3fd621d3e88b98f' => $vendorDir . '/symfony/polyfill-ctype/bootstrap.php',
@@ -15,13 +15,13 @@ return array(
     '8825ede83f2f289127722d4e842cf7e8' => $vendorDir . '/symfony/polyfill-intl-grapheme/bootstrap.php',
     'e69f7f6ee287b969198c3c9d6777bd38' => $vendorDir . '/symfony/polyfill-intl-normalizer/bootstrap.php',
     'b6b991a57620e2fb6b2f66f03fe9ddc2' => $vendorDir . '/symfony/string/Resources/functions.php',
-    'a1105708a18b76903365ca1c4aa61b02' => $vendorDir . '/symfony/translation/Resources/functions.php',
-    '253c157292f75eb38082b5acb06f3f01' => $vendorDir . '/nikic/fast-route/src/functions.php',
     '7b11c4dc42b3b3023073cb14e519683c' => $vendorDir . '/ralouphie/getallheaders/src/getallheaders.php',
     'c964ee0ededf28c96ebd9db5099ef910' => $vendorDir . '/guzzlehttp/promises/src/functions_include.php',
+    'a1105708a18b76903365ca1c4aa61b02' => $vendorDir . '/symfony/translation/Resources/functions.php',
+    '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
+    '253c157292f75eb38082b5acb06f3f01' => $vendorDir . '/nikic/fast-route/src/functions.php',
     'ff1b7935a93a4a9517db3ebe0533892a' => $vendorDir . '/opentracing/opentracing/src/OpenTracing/Tags.php',
     '0db36546c71c357f5ee70c39bb03966f' => $vendorDir . '/opentracing/opentracing/src/OpenTracing/Formats.php',
-    '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
     '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
     '198ca788260cba849b8d9061ef2afeb7' => $vendorDir . '/openzipkin/zipkin/src/Zipkin/Propagation/Id.php',
     '1078d5a0858ccde97f683b735c9f5473' => $vendorDir . '/openzipkin/zipkin/src/Zipkin/Timestamp.php',
---
+++ vendor/hyperf/service-governance/src/Listener/RegisterServiceListener.php
@@ -0,0 +1,133 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\ServiceGovernance\Listener;
+
+use Hyperf\Consul\Exception\ServerException;
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Contract\StdoutLoggerInterface;
+use Hyperf\Event\Contract\ListenerInterface;
+use Hyperf\Framework\Event\MainWorkerStart;
+use Hyperf\Server\Event\MainCoroutineServerStart;
+use Hyperf\ServiceGovernance\DriverManager;
+use Hyperf\ServiceGovernance\IPReaderInterface;
+use Hyperf\ServiceGovernance\ServiceManager;
+use Psr\Container\ContainerInterface;
+
+class RegisterServiceListener implements ListenerInterface
+{
+    /**
+     * @var StdoutLoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var ServiceManager
+     */
+    protected $serviceManager;
+
+    /**
+     * @var ConfigInterface
+     */
+    protected $config;
+
+    /**
+     * @var IPReaderInterface
+     */
+    protected $ipReader;
+
+    /**
+     * @var DriverManager
+     */
+    protected $governanceManager;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->logger = $container->get(StdoutLoggerInterface::class);
+        $this->serviceManager = $container->get(ServiceManager::class);
+        $this->config = $container->get(ConfigInterface::class);
+        $this->ipReader = $container->get(IPReaderInterface::class);
+        $this->governanceManager = $container->get(DriverManager::class);
+    }
+
+    public function listen(): array
+    {
+        return [
+            MainWorkerStart::class,
+            MainCoroutineServerStart::class,
+        ];
+    }
+
+    /**
+     * @param MainCoroutineServerStart|MainWorkerStart $event
+     */
+    public function process(object $event)
+    {
+        $continue = true;
+        while ($continue) {
+            try {
+                $services = $this->serviceManager->all();
+                $servers = $this->getServers();
+                foreach ($services as $serviceName => $serviceProtocols) {
+                    foreach ($serviceProtocols as $paths) {
+                        foreach ($paths as $service) {
+                            if (! isset($service['publishTo'], $service['server'])) {
+                                continue;
+                            }
+                            [$address, $port] = $servers[$service['server']];
+                            if ($governance = $this->governanceManager->get($service['publishTo'])) {
+                                if (! $governance->isRegistered($serviceName, $address, (int) $port, $service)) {
+                                    $governance->register($serviceName, $address, (int) $port, $service);
+                                }
+                            }
+                        }
+                    }
+                }
+                $continue = false;
+            } catch (ServerException $throwable) {
+                if (strpos($throwable->getMessage(), 'Connection failed') !== false) {
+                    $this->logger->warning('Cannot register service, connection of service center failed, re-register after 10 seconds.');
+                    sleep(10);
+                } else {
+                    throw $throwable;
+                }
+            }
+        }
+    }
+
+    protected function getServers(): array
+    {
+        $result = [];
+        $servers = $this->config->get('server.servers', []);
+        foreach ($servers as $server) {
+            if (! isset($server['name'], $server['host'], $server['port'])) {
+                continue;
+            }
+            if (! $server['name']) {
+                throw new \InvalidArgumentException('Invalid server name');
+            }
+            $host = $server['host'];
+            if (in_array($host, ['0.0.0.0', 'localhost'])) {
+                $host = $this->ipReader->read();
+            }
+            if (! filter_var($host, FILTER_VALIDATE_IP)) {
+                throw new \InvalidArgumentException(sprintf('Invalid host %s', $host));
+            }
+            $port = $server['port'];
+            if (! is_numeric($port) || ($port < 0 || $port > 65535)) {
+                throw new \InvalidArgumentException(sprintf('Invalid port %s', $port));
+            }
+            $port = (int) $port;
+            $result[$server['name']] = [$host, $port];
+        }
+        return $result;
+    }
+}
